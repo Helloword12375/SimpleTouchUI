0c99c4ab6f3c8b032695b1000da0d14b4a349fea
Helloword12375 <1914974951@qq.com>
2021-02-13 19:55:31 +0800

    first commit

---
+++ Script/Move.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+    public SimpleTouchUI SimpleTouch;
+    public Rigidbody _rigidbody;
+    public float speed;
+    private void Update()
+    {
+        _rigidbody.MovePosition(transform.position + (transform.forward * SimpleTouch.GetTouchPosition.y * Time.deltaTime * speed) +
+            (transform.right * SimpleTouch.GetTouchPosition.x * Time.deltaTime * speed));
+    }
+}
